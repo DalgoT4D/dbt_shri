c8a0c64b11a5d8c027f5e205174fd5fb95a6413b
siddhant <ssiddhant3030@gmail.com>
2023-03-14 01:45:54 +0530

    staff barcode

--- models/intermediate/staff_barcode_normalized.sql
+++ models/intermediate/staff_barcode_normalized.sql
@@ -10,6 +10,20 @@
 
 select
         _airbyte_ab_id,
-        _airbyte_emitted_at
+        _airbyte_emitted_at,
+        _airbyte_data ->> '_id' as id,
+        _airbyte_data ->> '_uuid' as uuid,
+        _airbyte_data ->> 'start' as starttime,
+        _airbyte_data ->> 'maintenance_numberid_in' as maintenance_numberid_in,
+        _airbyte_data ->> 'group_qx5fr96/name-timestamp' as nametimestamp,
+        _airbyte_data ->> 'group_qx5fr96/name-timestamp-formatted' as nametimestampformatted,
+        _airbyte_data ->> '__version__' as version,
+        _airbyte_data ->> 'meta/instanceID' as instanceID,
+        _airbyte_data ->> '_xform_id_string' as _xform_id_string,
+        _airbyte_data ->> '_status' as _status,
+        _airbyte_data ->> '_submission_time' as _submission_time,
+        _airbyte_data ->> '_submitted_by' as _submitted_by
 
 from {{ source('source_shri_surveys', 'staff_barcode') }}
+
+
