7e80662c8ce170874a496fe3e39b32d4800df178
siddhant <ssiddhant3030@gmail.com>
2023-03-20 21:10:44 +0530

    normalization code

--- models/intermediate/daily_issue_form_normalized.sql
+++ models/intermediate/daily_issue_form_normalized.sql
@@ -1,6 +1,3 @@
-#################### DAILY ISSUE FORM ###############################
-
-# Here we are pulling what we need for daily issue form and cleaning or renaming it. 
 
 {{ config(
   materialized='table',
@@ -11,22 +8,13 @@
 
 ) }}
 
-select
-        _airbyte_ab_id,
-        _airbyte_emitted_at,
-        _airbyte_data ->> '_uuid' as uuid,
-        _airbyte_data ->> 'starttime' as starttime,
-        _airbyte_data ->> 'endtime' as endtime,
-        _airbyte_data ->> 'timestamp' as timestamp,
-        _airbyte_data ->> 'timestamp-formatted' as timestampformatted,
-        _airbyte_data ->> '__version__' as version,
-        _airbyte_data ->> 'meta/instanceID' as instanceID,
-        _airbyte_data ->> '_xform_id_string' as _xform_id_string,
-        _airbyte_data ->> '_status' as _status,
-        _airbyte_data ->> '_submission_time' as _submission_time,
-        _airbyte_data ->> '_submitted_by' as _submitted_by,
-        _airbyte_data ->> 'shift_type' as shift_type,
-        _airbyte_data ->> 'minorissue_type' as minorissue_type
+
+{{
+    flatten_json(
+        model_name = source('source_shri_surveys', 'daily_issue_form'),
+        json_column = '_airbyte_data'
+    )
+}}
 
 
-from {{ source('source_shri_surveys', 'daily_issue_form') }}
+
