cb328fe62d075f3fb022260f58e25534db747094
siddhant <ssiddhant3030@gmail.com>
2023-03-27 23:47:33 +0530

    koboid link

--- models/intermediate/normalized/facility_koboid_link_normalized.sql
+++ models/intermediate/normalized/facility_koboid_link_normalized.sql
@@ -8,10 +8,17 @@
 ) }}
 
 
-{{
+with my_cte as ({{
     flatten_json(
         model_name = source('source_shri_surveys', 'facility_koboid_link'),
         json_column = '_airbyte_data'
     )
+}})
+
+{{ dbt_utils.deduplicate(
+    relation='my_cte',
+    partition_by='_id',
+    order_by='_id desc',
+   )
 }}
 
