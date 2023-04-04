7856975218d856a539ee3c83ae4634f562488c73
Abhishek N <abhisheknshetty97@gmail.com>
2023-04-04 09:42:02 +0530

    clean staff barcode data (#21)
    * clean staff barcode data
    
    * update staff_barcode prod
    
    * update staff_barcode

---
+++ models/intermediate/prod/staff_barcode_production.sql
@@ -0,0 +1,10 @@
+
+{{ config(
+  materialized='table',
+    schema='final'
+) }}
+
+-- Selecting specific columns from the 'staff_barcode' table
+
+SELECT userid, nametimestampformatted, datetime_auto_day, date_auto, _submission_time, _submitted_by, facility, yob, gender
+FROM {{ref('staff_barcode')}}
