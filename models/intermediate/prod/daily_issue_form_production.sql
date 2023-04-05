b2cea2fef422c816f8a59af19c2619cd3b8e4ae7
siddhant <ssiddhant3030@gmail.com>
2023-04-05 18:45:53 +0530

    daily issue form fix

--- models/intermediate/prod/daily_issue_form_production.sql
+++ models/intermediate/prod/daily_issue_form_production.sql
@@ -5,48 +5,72 @@
 ) }}
 
 
-with merged_normalized AS
-       (SELECT "facilityname" as "facility", "shift_type",
-       unnest(array['cleanliness_group_outage_pan_full', 
-                     'electrical_group_outage_wiring_hours',
-                     'plumbing_group_outage_pipe_full', 
-                     'plumbing_group_outage_tools',
-                     'plumbing_group_outage_socket', 
-                     'electrical_group_outage_boring_hours',
-                     'infrastructure_group_outage_wall_hours',
-                     'cleanliness_group_outage_drainblock_hours',
-                     'technology_group_outage_internet',
-                     'plumbing_group_outage_pipe_full',
-                     'cleanliness_group_outage_basinclean',
-                     'cleanliness_group_outage_odor_hours',
-                     'electrical_group_outage_boring_full',
-                     'electrical_group_outage_wiring_full',
-                     'supplies_group_outage_soap_hours'
+with my_cte AS (SELECT facilityname as facility, shift_type,
+       unnest(array['Electrical - bulb', 
+                    'Plumbing - basin tap', 
+                    'Technology - Internet', 
+                    'Electrical - wiring',
+                    'Electrical - boring',
+                    'Plumbing - toilet tap',
+                    'Technology - tablet',
+                    'Supplies - harpic',
+                    'Infrastructure - gate',
+                    'Supplies - soap',
+                    'Plumbing - pipe'
+       
        ]) AS issue,
-       unnest(array[cleanliness_group_outage_pan_full, 
-                     electrical_group_outage_wiring_hours,
-                     plumbing_group_outage_pipe_full, 
-                     plumbing_group_outage_tools,
-                     plumbing_group_outage_socket, 
-                     electrical_group_outage_boring_hours,
-                     infrastructure_group_outage_wall_hours,
-                     cleanliness_group_outage_drainblock_hours,
-                     technology_group_outage_internet,
-                     plumbing_group_outage_pipe_full,
-                     cleanliness_group_outage_basinclean,
-                     cleanliness_group_outage_odor_hours,
-                     electrical_group_outage_boring_full,
-                     electrical_group_outage_wiring_full,
-                     supplies_group_outage_soap_hours]) AS score
-FROM {{ref('daily_issue_form')}}
+       unnest(array[electrical_group_outage_bulb, 
+                    plumbing_group_outage_basintap, 
+                    technology_group_outage_internet, 
+                    electrical_group_outage_wiring, 
+                    electrical_group_outage_boring,
+                    plumbing_group_outage_toilettap,
+                    technology_group_outage_tablet, 
+                    supplies_group_outage_harpicetc,
+                    infrastructure_group_outage_gate,
+                    supplies_group_outage_soap, 
+                    plumbing_group_outage_pipe]) AS outage, 
+       unnest(array[electrical_group_bulb_fixed, 
+                    plumbing_group_basintap_fixed, 
+                    technology_group_internet_fixed, 
+                    electrical_group_wiring_fixed,
+                    electrical_group_boring_fixed,
+                    plumbing_group_toilettap_fixed, 
+                    technology_group_tablet_fixed, 
+                    supplies_group_harpicetc_fixed, 
+                    infrastructure_group_gate_fixed,
+                    supplies_group_soap_fixed,
+                    plumbing_group_pipe_fixed]) AS fixed, 
+       unnest(array[electrical_group_outage_bulb_full, 
+                    plumbing_group_outage_basintap_full, 
+                    technology_group_outage_internet_full, 
+                    electrical_group_outage_wiring_full,
+                    electrical_group_outage_boring_full,
+                    plumbing_group_outage_toilettap_full,
+                    technology_group_outage_tablet_full, 
+                    supplies_group_outage_harpicetc_full,
+                    infrastructure_group_outage_gate_full,
+                    supplies_group_outage_soap_full,
+                    plumbing_group_outage_pipe_full]) AS full_partial,
+       unnest(array[electrical_group_outage_bulb_hours, 
+                    plumbing_group_outage_basintap_hours, 
+                    technology_group_outage_internet_hours, 
+                    electrical_group_outage_wiring_hours,
+                    electrical_group_outage_boring_hours,
+                    plumbing_group_outage_toilettap_hours, 
+                    technology_group_outage_tablet_hours,
+                    supplies_group_outage_harpicetc_hours,
+                    infrastructure_group_outage_gate_hours,
+                    supplies_group_outage_soap_hours,
+                    plumbing_group_outage_pipe_hours]) AS num_hours
+       FROM {{ref('daily_issue_form')}}
 )
 
 
 
 select *,
-        case 
-            when score = '0' then 'NO' 
-            when score = '1' then 'YES'
-        END as shutdown
-from  merged_normalized
-where score is NOT NULL
+      Case 
+        When outage = '0' then 'NO'
+        When outage = '1' then 'YES'
+      END as shutdown
+from my_cte where outage is not null
