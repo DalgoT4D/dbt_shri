0bba59d740ac4df7cd308f13112e88f8a43f2111
siddhant <ssiddhant3030@gmail.com>
2023-04-10 18:07:43 +0530

    cleaning

--- models/prod/raw/enrollment_production.sql
+++ models/prod/raw/enrollment_production.sql
@@ -5,5 +5,5 @@
 
 -- Selecting specific columns from the 'enrollment' table
 
-SELECT yob, gender, id_num as userid, formtimestampformatted, _submission_time, _submitted_by, facility_updated AS facility, date_enrollment, age_years, age_cat
+SELECT yob, gender, id_num as userid, _submitted_by, facility_updated AS facility, date_enrollment, age_years, age_cat
 FROM {{ref('enrollment')}}
