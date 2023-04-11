9ce233a6433e2940edc8493826d501359503d1e2
siddhant <ssiddhant3030@gmail.com>
2023-04-11 11:34:38 +0530

    adding comments

--- models/prod/raw/daily_issue_form_clean.sql
+++ models/prod/raw/daily_issue_form_clean.sql
@@ -2,11 +2,12 @@
   materialized='table'
 ) }}
 
-
+-- adding the _id here to track each record. 
 select _id, facilityname, shift_type, 
  Case
   when minorissue_type is not null then true
   else false
  end as any_issue,
  
+ -- changing the stattime to date here
  starttime::date FROM {{ref('daily_issue_form')}}
