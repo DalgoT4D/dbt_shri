e1af8fda4f499292e0ae14dbaac7f2d8bae76529
Siddhant Singh <ssiddhant3030@gmail.com>
2023-04-18 17:43:03 +0530

    Feature/cleaning (#24)
    * enrollment no duplicates name changes
    
    * not needed fields removed
    
    * data_work cleaning
    
    * cleaning the dbt forms

--- tests/enrollment_test.sql
+++
@@ -1,8 +0,0 @@
-version: 2
-
-tests:
-  - name: test_no_duplicate_values_in_column
-  
-    run:
-      - expect(select count(*) from {{ ref('enrollment_production_dedup_remove') }}) = 
-        select count(distinct 'dataid') from {{ ref('enrollment_production_dedup_remove') }}
