73794d88b53827ef48f50dbd424b4e143828d025
siddhant <ssiddhant3030@gmail.com>
2023-04-27 21:21:25 +0530

    cleaning

--- models/prod/raw/daily_issue_clean.sql
+++ models/prod/raw/daily_issue_clean.sql
@@ -3,6 +3,21 @@
 ) }}
 
 
-{{ dbt_utils.union_relations(
+with my_cte as ({{ dbt_utils.union_relations(
     relations=[ref('daily_issue_form_dashboard'),ref('daily_issue_form_aggregate')]
-) }}
+) }})
+
+
+select 
+    _id,
+    facility,
+    issue,
+    shift_type,
+    fixed,
+    any_issue,
+    full_partial,
+    num_hours,
+    shutdown,
+    date_auto
+
+from my_cte
