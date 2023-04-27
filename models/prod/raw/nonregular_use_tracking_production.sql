9daa635fc333bea9076348ce25e537b607da17c8
siddhant <ssiddhant3030@gmail.com>
2023-04-27 18:19:41 +0530

    cleaning

--- models/prod/raw/nonregular_use_tracking_production.sql
+++ models/prod/raw/nonregular_use_tracking_production.sql
@@ -4,5 +4,13 @@
 
 -- Selecting specific columns from the 'nonregular_use_tracking' table
 
-SELECT starttime, who, child_girl_number, child_boy_number, passerby_woman_number, passerby_man_number, date_auto, facility, time_auto
+SELECT starttime, 
+       who, 
+       child_girl_number, 
+       child_boy_number, 
+       passerby_woman_number, 
+       passerby_man_number, 
+       date_auto, 
+       facility, 
+       time_auto
 FROM {{ref('nonregular_use_tracking')}}
--- models/prod/raw/staff_barcode_production.sql
+++ models/prod/raw/staff_barcode_production.sql
@@ -5,5 +5,13 @@
 
 -- Selecting specific columns from the 'staff_barcode' table
 
-SELECT userid, nametimestampformatted, datetime_auto_day, date_auto, _submission_time, _submitted_by, facility, yob, gender
+SELECT userid, 
+       nametimestampformatted, 
+       datetime_auto_day, 
+       date_auto, 
+       _submission_time, 
+       _submitted_by, 
+       facility, 
+       yob, 
+       gender
 FROM {{ref('staff_barcode')}}
