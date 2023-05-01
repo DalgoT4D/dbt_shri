991e8d6183e5ae0e2a7d9cd1f298dc4dbad1a03e
siddhant <ssiddhant3030@gmail.com>
2023-05-01 15:29:19 +0530

    added more comments

--- models/intermediate/cleaned/use_tracking.sql
+++ models/intermediate/cleaned/use_tracking.sql
@@ -5,6 +5,7 @@
 ) }}
 
 # This model is to join a table with koboid link normalized
+# Read more about with cte here https://www.postgresql.org/docs/current/queries-with.html
 with cte as(  
         select
             numberid as userid,
