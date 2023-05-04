ed3339ca650d8d6a56373af75ea49e6732c7b33f
siddhant <ssiddhant3030@gmail.com>
2023-05-04 14:23:23 +0530

    added comments

--- models/prod/raw/daily_issue_clean.sql
+++ models/prod/raw/daily_issue_clean.sql
@@ -3,6 +3,13 @@
 ) }}
 
 
+-- To test the data you can run this query
+
+-- SELECT facility, COUNT(*) as count
+-- FROM prod.daily_issue_clean
+-- GROUP BY facility;
+
+
 with my_cte as ({{ dbt_utils.union_relations(
     relations=[ref('daily_issue_form_dashboard'),ref('daily_issue_form_aggregate')]
 ) }})
