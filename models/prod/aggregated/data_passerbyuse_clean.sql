a259f57f678592644f543359c92a62f46afd0009
siddhant <ssiddhant3030@gmail.com>
2023-05-16 12:30:37 +0530

    daily issue

--- models/prod/aggregated/data_passerbyuse_clean.sql
+++ models/prod/aggregated/data_passerbyuse_clean.sql
@@ -6,9 +6,9 @@
 
 SELECT facility, date_auto, 
 sum(coalesce(child_girl_number, 0)) as girl_number,
-sum(coalesce(passerby_man_number, 0)) as man_number,
+sum(coalesce(child_boy_number, 0)) as boy_number,
 sum(coalesce(passerby_woman_number, 0)) as woman_number,
-sum(coalesce(child_boy_number, 0)) as boy_number
+sum(coalesce(passerby_man_number, 0)) as man_number
 FROM {{ref('nonregular_use_tracking_production')}}
 group by date_auto, facility
 order by date_auto asc
--- models/prod/aggregated/usetracking_dashboard.sql
+++ models/prod/aggregated/usetracking_dashboard.sql
@@ -18,20 +18,29 @@ sum(
  from {{ref('use_tracking')}}
  GROUP BY
     date_auto, facility
- order by date_auto desc) 
+ order by date_auto desc)
 
- select a.*,
+select 
+ coalesce(a.facility, b.facility) as facility, 
+ coalesce(a.date_auto, b.date_auto) as date_auto,
  coalesce(b.woman_number, 0) as woman_number, 
  coalesce(b.girl_number, 0) as girl_number, 
  coalesce(b.man_number, 0) as man_number, 
  coalesce(b.boy_number, 0) as boy_number,
- a.men_regular_number + coalesce(b.man_number, 0) as men_use,
- a.women_regular_number + coalesce(b.woman_number, 0) as women_use,
+ coalesce(a.men_regular_number, 0) + coalesce(b.man_number, 0) as men_use,
+ coalesce(a.women_regular_number, 0) + coalesce(b.woman_number, 0) as women_use,
  coalesce(b.boy_number, 0) as boys_use,
  coalesce(b.girl_number, 0) as girls_use,
- coalesce(b.woman_number, 0) + coalesce(b.girl_number, 0) + coalesce(b.man_number,0 ) + coalesce(b.boy_number, 0) + a.men_regular_number + a.women_regular_number  as total_use
+ coalesce(b.woman_number, 0) + coalesce(b.girl_number, 0) + coalesce(b.man_number,0 ) + coalesce(b.boy_number, 0) + coalesce(a.men_regular_number, 0) + coalesce(a.women_regular_number, 0)  as total_use
  
  from my_cte as a
- left join {{ref('data_passerbyuse_clean')}} as b
+ full outer join {{ref('data_passerbyuse_clean')}} as b
  on a.facility = b.facility and a.date_auto = b.date_auto 
+
+  
+
+
+ 
+ 
+
  
