4bb00cc1ef18e5360407adef00254b1d62e72d82
siddhant <ssiddhant3030@gmail.com>
2023-07-02 13:19:52 +0530

    name fixes

--- models/prod/aggregated/enrollment_aggregated.sql
+++ models/prod/aggregated/enrollment_aggregated.sql
@@ -6,5 +6,12 @@
 
 -- Selecting specific columns from the 'enrollment' table
 
-SELECT yob, gender, id_num as userid, _submitted_by, facility_updated AS facility, date_enrollment, age_years, age_cat
+SELECT yob, 
+       gender, 
+       id_num as userid, 
+       _submitted_by, 
+       facility_updated AS facility, 
+       date_enrollment, 
+       age_years, 
+       age_cat
 FROM {{ref('enrollment')}}
