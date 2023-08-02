73ea82c5d262cf35b338f7f8f9dd119682cdc46a
siddhant <ssiddhant3030@gmail.com>
2023-08-02 17:25:29 +0530

    new time added

--- models/intermediate/cleaned/nonregular_use_tracking.sql
+++ models/intermediate/cleaned/nonregular_use_tracking.sql
@@ -18,7 +18,7 @@ with cte as (SELECT
 
     -- creating a new column date_auto from _submission_time and convert to date format
     to_date(starttime, 'YYYY-MM-DD') AS date_auto,
-    TO_TIMESTAMP(SUBSTRING(starttime, 1, 23), 'YYYY-MM-DDTHH24:MI:SS')::TIME AS time_auto,
+    SUBSTRING(starttime FROM 'T(\d{2}:\d{2}:\d{2})')::time AS time_auto,
 
     -- selecting all columns from nonregular_use_tracking_normalized except the ones listed
     {{ dbt_utils.star(from = ref('nonregular_use_tracking_normalized'), 
