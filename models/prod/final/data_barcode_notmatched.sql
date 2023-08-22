33d62ae8110e041e355b97c1e9428fc4f621fb1a
siddhant <ssiddhant3030@gmail.com>
2023-08-22 20:53:04 +0530

    dateauto

--- models/prod/final/data_barcode_notmatched.sql
+++ models/prod/final/data_barcode_notmatched.sql
@@ -9,4 +9,4 @@ facility,
 date_auto, 
 TO_TIMESTAMP(datetime_auto_day, 'YYYY-MM-DD HH24:MI:SS')::TIME as time_auto
 FROM {{ref('staff_barcode')}} where position is null
-order by date_auto
+order by date_auto desc
