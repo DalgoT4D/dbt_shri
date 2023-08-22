3995dd20bf7f22d23ffcff147cb45d977cac5d6f
siddhant <ssiddhant3030@gmail.com>
2023-08-22 20:47:31 +0530

    staff barcode new model

---
+++ models/prod/final/data_barcode_notmatched.sql
@@ -0,0 +1,7 @@
+select distinct 
+userid, 
+position, 
+facility, 
+date_auto, 
+TO_TIMESTAMP(datetime_auto_day, 'YYYY-MM-DD HH24:MI:SS')::TIME as time_auto
+FROM {{ref('staff_barcode')}} where position is null
