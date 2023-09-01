e9e01976d5a4b43f7485c4bc990ff33c0af2a830
siddhant <ssiddhant3030@gmail.com>
2023-09-02 00:59:00 +0530

    use tracking test

---
+++ models/intermediate/cleaned/use_tracking_test.sql
@@ -0,0 +1,12 @@
+with cte as (select  _id, to_date
+            ( coalesce ( 
+            begin_group_ajqop6jqs_name_timestamp_formatted, 
+            begin_group_kthfwkunp_name_timestamp_formatted, 
+            begin_group_5xw8upowl_name_timestamp_formatted, 
+            name_timestamp_formatted), 'YYYY-MM-DD') as date_auto,
+            _submitted_by,
+            numberid
+            
+         from {{ ref('use_tracking_normalized') }} )
+            
+select * from cte where date_auto = '2021-11-07'
