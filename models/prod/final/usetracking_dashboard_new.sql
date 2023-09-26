1d582fc54e647ef8d8cfdeaf694bbc6d2f14a54b
siddhant <ssiddhant3030@gmail.com>
2023-09-26 19:41:39 +0530

    added new logic

--- models/prod/final/usetracking_dashboard_new.sql
+++ models/prod/final/usetracking_dashboard_new.sql
@@ -4,7 +4,7 @@
 
 
 select date_auto, facility, 
-unnest(array['men_use','women_use','girls_use','boys_use']) as user,
+unnest(array['men','women','girls','boys']) as user,
 unnest(array[men_use,women_use,girls_use,boys_use]) as use
 
 from {{ref('usetracking_dashboard')}}
