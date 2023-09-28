9863343e60636175740c07092b16101a91e01ce6
siddhant <ssiddhant3030@gmail.com>
2023-09-28 17:05:05 +0530

    remove nulls from the final table

--- models/prod/final/daily_issue_dashboard.sql
+++ models/prod/final/daily_issue_dashboard.sql
@@ -10,16 +10,16 @@ with my_cte as (({{ dbt_utils.union_relations(
 ) }}))
 
 select 
-    _id,
-    facility,
+    COALESCE(_id::text, '') as _id,
+    COALESCE(facility, '') as facility,
     date_auto,
     time_auto,
-    category,
-    shift_type,
-    issue,
-    fixed,
-    full_partial,
-    num_hours,
-    shutdown
+    COALESCE(category, '') as category,
+    COALESCE(shift_type, '') as shift_type,
+    COALESCE(issue, '') as issue,
+    COALESCE(fixed, '') as fixed,
+    COALESCE(full_partial, '') as full_partial,
+    COALESCE(num_hours::text, '') as num_hours,
+    COALESCE(shutdown, '') as shutdown
 
 from my_cte
