de2592e6448086f67fd77de37166b30b5dcb5c79
siddhant <ssiddhant3030@gmail.com>
2023-09-28 19:28:56 +0530

    usetracking report

---
+++ models/prod/final/usetracking_daily_report.sql
@@ -0,0 +1,14 @@
+{{ config(
+  materialized='table'
+) }}
+
+select 
+total_use, 
+men_regular_number + women_regular_number as regular_users,
+girl_number + boy_number + man_number + woman_number as non_regular_users, 
+men_use as men, 
+women_use as women,
+boys_use as boys,
+girls_use as girls
+from 
+FROM {{ref('usetracking_dashboard')}}
