24b78a335914d48033f1b7eee2160273f44918c8
siddhant <ssiddhant3030@gmail.com>
2023-10-04 22:09:06 +0530

    daily report clean

--- models/prod/final/usetracking_daily_report.sql
+++ models/prod/final/usetracking_daily_report.sql
@@ -6,8 +6,8 @@ select
 total_use, 
 men_regular_number + women_regular_number as regular_users,
 girl_number + boy_number + man_number + woman_number as non_regular_users, 
-men_use as men, 
-women_use as women,
+man_number as men, 
+woman_number as women,
 boys_use as boys,
 girls_use as girls
 FROM {{ref('usetracking_dashboard')}}
