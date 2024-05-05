79fa6ea08fd174be6cd65cd2fefda73172ee5109
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-05 14:36:56 +0530

    no reports data update

--- models/intermediate/cleaned/daily_issue_form.sql
+++ models/intermediate/cleaned/daily_issue_form.sql
@@ -60,4 +60,20 @@ with
     ON daily_issue._submitted_by = form_kd.kobo_username
    )
 
-select * from join_all_tables where date_auto is not null and time_auto is not null
+select * from join_all_tables 
+where date_auto is not null 
+and time_auto is not null
+and (facilityname = 'Dundibagh' AND date_auto >= '2022-02-19')
+        OR (facilityname = 'Basgoda' AND date_auto >= '2022-02-19')
+        OR (facilityname = 'Gomia' AND date_auto >= '2022-02-19')
+        OR (facilityname = 'Azad Nagar' AND date_auto >= '2022-08-23')
+        OR (facilityname = 'North Basgoda' AND date_auto >= '2022-08-23')
+        OR (facilityname = 'Peterbaar' AND date_auto >= '2023-01-14')
+        OR (facilityname = 'Vurahi' AND date_auto >= '2023-03-02')
+        OR (facilityname = 'Jaridih CSR' AND date_auto >= '2023-07-01')
+        OR (facilityname = 'Jaridih SBM' AND date_auto >= '2023-07-01')
+        OR (facilityname = 'Kasmar' AND date_auto >= '2023-07-01')
+        OR (facilityname = 'Nemua' AND date_auto >= '2023-10-29')
+        OR (facilityname = 'Bela Museri' AND date_auto >= '2023-12-04')
+        OR (facilityname = 'Bairo' AND date_auto >= '2023-11-01')
+        OR (facilityname = 'Karanpur' AND date_auto >= '2023-10-13') 
--- models/prod/final/no_reports_data.sql
+++ models/prod/final/no_reports_data.sql
@@ -3,43 +3,20 @@
 ) }}
 
 
-WITH date_range AS (
-    SELECT generate_series(
-        '2020-01-01'::date,  -- Start date
-        '2024-12-31'::date,  -- End date
-        '1 day'::interval    -- Interval of 1 day
-    ) AS date_auto
-),
-facility_list AS (
+WITH DateRange AS (
+    SELECT generate_series(MIN(date_auto), MAX(date_auto), interval '1 day') AS date
+    FROM prod_final.daily_issue_clean
+), Facilities AS (
     SELECT DISTINCT facility
-    FROM {{ref('daily_issue_clean')}}
-),
-cross_product AS (
-    SELECT dr.date_auto, fl.facility
-    FROM date_range dr
-    CROSS JOIN facility_list fl
-),
-issues_reported AS (
-    SELECT 
-        date_auto,
-        facility,
-        COUNT(issue) AS reported_issues_count
-    FROM 
-        {{ref('daily_issue_clean')}}
-    WHERE
-        issue IS NOT NULL AND issue <> '' -- This assumes an empty 'issue' column means no issue reported
-    GROUP BY 
-        date_auto,
-        facility
+    FROM prod_final.daily_issue_clean
+), FacilityDates AS (
+    SELECT f.facility, d.date
+    FROM Facilities f
+    CROSS JOIN DateRange d
 )
-
-SELECT 
-    cast(cp.date_auto as date) as date_auto,
-    cp.facility,
-    COALESCE(ir.reported_issues_count, 0) AS no_of_reported_issues
-FROM 
-    cross_product cp
-LEFT JOIN issues_reported ir ON cp.date_auto = ir.date_auto AND cp.facility = ir.facility
-ORDER BY
-    cp.facility, 
-    cp.date_auto
+SELECT fd.facility, CAST(fd.date AS date) AS date, COUNT(d._id) AS submission_count
+FROM FacilityDates fd
+LEFT JOIN prod_final.daily_issue_clean d ON fd.facility = d.facility AND fd.date = d.date_auto
+WHERE fd.date <= CURRENT_DATE  -- Adding the WHERE clause here
+GROUP BY fd.facility, fd.date
+HAVING COUNT(d._id) < 2
