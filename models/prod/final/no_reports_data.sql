954da81cb56e56aa59d919a5787601aef90b6050
Siddhant Singh <ssiddhant3030@gmail.com>
2024-05-31 21:59:42 +0530

    fix the no reports data

--- models/prod/final/no_reports_data.sql
+++ models/prod/final/no_reports_data.sql
@@ -13,10 +13,14 @@ WITH DateRange AS (
     SELECT f.facility, d.date
     FROM Facilities f
     CROSS JOIN DateRange d
+), SubmissionCounts AS (
+    SELECT facility, date_auto, COUNT(*) AS submission_count
+    FROM prod_final.daily_issue_clean
+    GROUP BY facility, date_auto
 )
-SELECT fd.facility, CAST(fd.date AS date) AS date, COUNT(d._id) AS submission_count
+SELECT fd.facility, CAST(fd.date AS date) AS date, COALESCE(sc.submission_count, 0) AS submission_count
 FROM FacilityDates fd
-LEFT JOIN prod_final.daily_issue_clean d ON fd.facility = d.facility AND fd.date = d.date_auto
-WHERE fd.date <= CURRENT_DATE  
-GROUP BY fd.facility, fd.date
-HAVING COUNT(d._id) < 2
+LEFT JOIN SubmissionCounts sc ON fd.facility = sc.facility AND fd.date = sc.date_auto
+WHERE fd.date <= CURRENT_DATE
+AND COALESCE(sc.submission_count, 0) < 2
+ORDER BY fd.facility, fd.date 
