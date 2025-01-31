86c269d6da6e80d3e9dc986e39bf7d3dc7a7e5b4
Siddhant Singh <ssiddhant3030@gmail.com>
2025-01-31 23:28:52 +0530

    added unit test case for the model

--- models/prod/final/no_reports_data.sql
+++ models/prod/final/no_reports_data.sql
@@ -2,14 +2,18 @@
   materialized='table'
 ) }}
 
-
 WITH DateRange AS (
-    SELECT generate_series(MIN(DATE(date_auto)), MAX(DATE(date_auto)), interval '1 day') AS date
-    FROM {{ref('daily_issue_clean')}}
+    -- Generate date series without timestamps
+    SELECT generate_series(
+        MIN(DATE(date_auto)), 
+        MAX(DATE(date_auto)), 
+        INTERVAL '1 day'
+    )::DATE AS date
+    FROM {{ ref('daily_issue_clean') }}
 ), 
 Facilities AS (
     SELECT DISTINCT facility
-    FROM {{ref('daily_issue_clean')}}
+    FROM {{ ref('daily_issue_clean') }}
 ), 
 FacilityDates AS (
     SELECT f.facility, d.date
@@ -17,16 +21,22 @@ FacilityDates AS (
     CROSS JOIN DateRange d
 ), 
 SubmissionCounts AS (
-    SELECT facility, DATE(date_auto) AS date_auto, COUNT(*) AS submission_count
-    FROM {{ref('daily_issue_clean')}}
+    SELECT 
+        facility, 
+        DATE(date_auto) AS date_auto,  -- Ensure date is without time
+        COUNT(*) AS submission_count
+    FROM {{ ref('daily_issue_clean') }}
     GROUP BY facility, DATE(date_auto)
 )
 
-SELECT fd.facility, fd.date, COALESCE(sc.submission_count, 0) AS submission_count
+SELECT 
+    fd.facility, 
+    fd.date::DATE AS date,  -- Ensure final output is a pure DATE type
+    COALESCE(sc.submission_count, 0) AS submission_count
 FROM FacilityDates fd
 LEFT JOIN SubmissionCounts sc 
 ON fd.facility = sc.facility 
 AND fd.date = sc.date_auto
-WHERE fd.date <= CURRENT_DATE
+WHERE fd.date <= CURRENT_DATE::DATE  -- Ensure comparison is done on DATE type
 AND COALESCE(sc.submission_count, 0) < 2
 ORDER BY fd.facility, fd.date
