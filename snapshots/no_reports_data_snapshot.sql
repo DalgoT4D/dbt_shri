8391e818b615d92e86c9ded4f3316e52d5266e9b
Siddhant Singh <ssiddhant3030@gmail.com>
2025-01-31 21:52:53 +0530

    new logic added

---
+++ snapshots/no_reports_data_snapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot facility_submission_snapshot %}
+
+{{ config(
+    target_schema="snapshots",
+    unique_key="date",
+    strategy="timestamp",
+    updated_at="date"  
+) }}
+
+SELECT 
+    facility, 
+    date, 
+    submission_count
+FROM {{ ref('no_reports_data') }}
+
+{% endsnapshot %}
