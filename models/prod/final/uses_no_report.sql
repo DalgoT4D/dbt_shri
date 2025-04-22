0f659d826226fc7bb68c6c94d10d175e25510936
Pratiksha Rao <pratz.rao@gmail.com>
2025-04-22 09:16:24 +0530

    changed model uses_no_reports to fill missing dates only after date of facility opening

--- models/prod/final/uses_no_report.sql
+++ models/prod/final/uses_no_report.sql
@@ -2,40 +2,37 @@
   materialized='table'
 ) }}
 
--- Determine the dynamic range of dates based on the existing data
-WITH cte as (WITH dynamic_range AS (
-  SELECT MIN(date_auto::date) AS start_date, MAX(date_auto::date) AS end_date
-  FROM {{ref('usetracking_dashboard_new')}}
+WITH Facilities AS (
+    SELECT DISTINCT facility
+    FROM {{ ref('usetracking_dashboard_new') }}
 ),
-date_series AS (
-  SELECT generate_series(start_date, end_date, '1 day'::interval) AS date
-  FROM dynamic_range
+FacilityDates AS (
+    SELECT 
+        f.facility, 
+        generate_series(
+            (SELECT MIN(DATE(date_auto)) 
+             FROM {{ ref('usetracking_dashboard_new') }} 
+             WHERE facility = f.facility),
+            CURRENT_DATE,
+            INTERVAL '1 day'
+        )::DATE AS date
+    FROM Facilities f
 ),
-facilities AS (
-  SELECT DISTINCT facility FROM {{ref('usetracking_dashboard_new')}}
-),
-all_combinations AS (
-  SELECT d.date, f.facility
-  FROM date_series d
-  CROSS JOIN facilities f
-),
-data_counts AS (
-  SELECT date_auto::date AS date_auto, facility, COUNT(*) AS num_entries
-  FROM {{ref('usetracking_dashboard_new')}}
-  GROUP BY date_auto::date, facility
+SubmissionCounts AS (
+    SELECT 
+        facility, 
+        DATE(date_auto) AS date_auto,
+        COUNT(*) AS submission_count
+    FROM {{ ref('usetracking_dashboard_new') }}
+    GROUP BY facility, DATE(date_auto)
 )
-SELECT ac.date, ac.facility
-FROM all_combinations ac
-LEFT JOIN data_counts dc 
-ON ac.date = dc.date_auto AND ac.facility = dc.facility
-WHERE dc.num_entries IS NULL OR dc.num_entries = 0)
 
-SELECT
-cast(date as date) as date_auto, 
-facility 
-from cte
-WHERE date <= CURRENT_DATE 
-
-
-
-
+SELECT 
+    fd.date AS date_auto,
+    fd.facility
+FROM FacilityDates fd
+LEFT JOIN SubmissionCounts sc 
+    ON fd.facility = sc.facility AND fd.date = sc.date_auto
+WHERE fd.date <= CURRENT_DATE
+  AND COALESCE(sc.submission_count, 0) = 0
+ORDER BY fd.facility, fd.date
