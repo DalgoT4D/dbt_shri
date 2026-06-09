d3d2595423caac08e2c002b25967aeeb8a9577b0
Pratiksha Rao <pratz.rao@gmail.com>
2026-06-09 14:20:32 +0530

    Fix Sayal North Patratu Kobo mapping

--- models/intermediate/normalized/facility_koboid_link_normalized.sql
+++ models/intermediate/normalized/facility_koboid_link_normalized.sql
@@ -24,4 +24,35 @@ deduplicated AS (
     ) }}
 )
 
-SELECT * FROM deduplicated
+SELECT
+    _airbyte_raw_id,
+    _id,
+    CASE
+        -- Correct the Kobo username typo so Sayal North submissions join downstream models.
+        WHEN facilityname = 'Sayal North Patratu'
+            AND kobo_username = 'savalnorthratratu'
+            THEN 'sayalnorthpatratu'
+        ELSE kobo_username
+    END AS kobo_username,
+    start,
+    starttime,
+    meta_deprecatedid,
+    endtime,
+    _validation_status,
+    "end",
+    formhub_uuid,
+    meta_instancename,
+    _uuid,
+    _xform_id_string,
+    _tags,
+    _submission_time,
+    meta_rootuuid,
+    _geolocation,
+    facilityname,
+    _status,
+    meta_instanceid,
+    _attachments,
+    _submitted_by,
+    _notes,
+    __version__
+FROM deduplicated
