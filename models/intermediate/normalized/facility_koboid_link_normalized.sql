b6f464eb67a966becb2b109f46536e1a9d3ff9b8
Pratiksha Rao <pratz.rao@gmail.com>
2026-06-25 11:44:06 +0530

    Fix Jaridih East Kobo username mapping

--- models/intermediate/normalized/facility_koboid_link_normalized.sql
+++ models/intermediate/normalized/facility_koboid_link_normalized.sql
@@ -5,6 +5,18 @@
     materialized='table'
 ) }}
 
+{% set survey_methods_query %}
+SELECT DISTINCT(jsonb_object_keys(data)) AS column_name
+FROM {{ source('source_shri_surveys', 'facility_koboid_link') }}
+{% endset %}
+
+{% if execute %}
+{% set survey_methods_results = run_query(survey_methods_query) %}
+{% set facility_link_keys = survey_methods_results.columns[0].values() %}
+{% else %}
+{% set facility_link_keys = [] %}
+{% endif %}
+
 WITH flattened AS (
     SELECT * FROM (
         {{
@@ -24,4 +36,28 @@ deduplicated AS (
     ) }}
 )
 
-SELECT * FROM deduplicated
+SELECT
+    _airbyte_raw_id,
+    {% for column_name in facility_link_keys %}
+    {%- set flattened_column_name = column_name
+        | replace('begin_group_yeQ4kl9Kt/', '')
+        | replace('begin_group_G8GvBDlis/', '')
+        | replace('/', '_')
+        | replace('-', '_')
+        | lower -%}
+    {% if flattened_column_name == 'kobo_username' %}
+    CASE
+        -- Correct known Kobo username typos so submissions join downstream models.
+        WHEN facilityname = 'Sayal North Patratu'
+            AND kobo_username = 'savalnorthratratu'
+            THEN 'sayalnorthpatratu'
+        WHEN facilityname = 'PM SHRI Middle school Jaridih East'
+            AND kobo_username = 'middeschooljaridiheast'
+            THEN 'middleschooljaridiheast'
+        ELSE kobo_username
+    END AS kobo_username
+    {% else %}
+    {{ adapter.quote(flattened_column_name) }}
+    {% endif %}{% if not loop.last %},{% endif %}
+    {% endfor %}
+FROM deduplicated
